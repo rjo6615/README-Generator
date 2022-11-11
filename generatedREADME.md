11aed3b714cf134c8803bf0d09826f2fd4f951b6
rjo6615 <robertjobernier@gmail.com>
2022-11-11 13:31:38 -0500

    testing license link

---
+++ generatedREADME.md
@@ -0,0 +1,42 @@
+
+    
+# fdsafdsa
+
+## Description
+fdsfdsa
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+fdsafd
+
+## Usage
+
+fdsafd
+
+## Credits
+
+fdsafdsa
+
+## License
+
+![badge](https://img.shields.io/badge/license-Apache-lightblue)
+[Link to Apache license](https://www.apache.org/licenses/LICENSE-2.0)
+
+## Features
+
+fdsafdas
+
+## How to Contribute
+
+fdsafdas
+
+## Tests
+
+fdsafdas
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,18 +9,23 @@ function renderLicenseBadge(license) {
     }
 }
 
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if(license == "None"){
+        licenseLink = "";
+    } else if(license == "Apache"){
+        licenseLink = "[Link to Apache license](https://www.apache.org/licenses/LICENSE-2.0)"
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
     renderLicenseBadge(answers.license);
+    renderLicenseLink(answers.license);
     return`
     
 # ${answers.title}
@@ -50,6 +55,7 @@ ${answers.credits}
 ## License
 
 ${licenseBadge}
+${licenseLink}
 
 ## Features
 
