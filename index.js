ac4535d00b9ee034a420e67f8daa910c9f621536
rjo6615 <robertjobernier@gmail.com>
2022-11-11 11:54:47 -0500

    added test prompt and file writing code

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateReadme = require("./utils/generateReadme")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+function questions(){
+inquirer
+.prompt([
+    {
+        type: "input", 
+        name: "name", 
+        message: "What is your name?"
+    },
+    {
+        type: "input", 
+        name: "bio", 
+        message: "Write a bio"
+    },
+    {
+        type: "input", 
+        name: "location", 
+        message: "What is your location?"
+    },
+    {
+        type: "input", 
+        name: "linkedIn", 
+        message: "What is your LinkedIn?"
+    },
+    {
+        type: "input", 
+        name: "gitHub", 
+        message: "What is your GitHub?"
+    }
+])
+.then((answer) => {
+const createReadme = generateReadme(answers);
+const fileName = "./generatedREADME.md";
+fs.writeToFile(fileName, createReadme, (error) => 
+error ? console.error(error) : console.log("Saved!"));
+})
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// const createReadme = generateReadme(answers);
+// const fileName = "./generatedREADME.md";
+// function writeToFile(fileName, createReadme) {
+
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions();
+}
 
 // Function call to initialize app
 init();
